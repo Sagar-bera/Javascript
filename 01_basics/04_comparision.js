485ac83f0623e93832a60368ff99b37a3ad03d52
Sagar Bera <155678720+Sagar-bera@users.noreply.github.com>
2024-08-16 05:44:34 +0000

    datatype comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,17 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2==1);
+
+console.log("2"==1);
+console.log("2">=1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0);
+console.log(undefined>=0);
+
+// === check the data type
+
+console.log("2"===2);
