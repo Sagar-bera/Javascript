8856c474a6c981e8573af72fe2534ed152b6d334
Sagar Bera <155678720+Sagar-bera@users.noreply.github.com>
2025-03-13 03:44:54 +0000

    Control Flow

---
+++ 04_control_Flow/switch.js
@@ -0,0 +1,13 @@
+const month = 2;
+switch (month) {
+    case 1: console.log("January");
+        break;
+    case 2: console.log("February");
+        break;
+    case 3: console.log("march");
+        break;
+
+    default: console.log("Default case match");
+    
+      break;  
+}
