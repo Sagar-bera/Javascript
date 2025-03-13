c8e1f376ec87bb1a3d299ef511ab19a565054e0f
Sagar Bera <155678720+Sagar-bera@users.noreply.github.com>
2025-03-13 06:06:20 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+let i = 0;
+while (i<10) {
+    console.log(i+1);
+    i++;
+    
+}
+
+const myarr = ["Sagar", "Pratik", "Ashok", "Utsab"]
+
+arr=0;
+while (arr<myarr.length) {
+    console.log(myarr[arr]);
+     arr++;    
+}
+
+let score=11;
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score<=10);
